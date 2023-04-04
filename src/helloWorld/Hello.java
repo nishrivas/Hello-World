9f83b8e5447c012e97d4d696b9b45556541301c8
Shrivas <nitu.shrivas@accenture.com>
2023-04-04 15:02:03 +0530

    Initial Commit

---
+++ src/helloWorld/Hello.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int value1=1,value2=2;
+		 
+		System.out.println(value2);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nitu.shrivas
+ *
+ */
+module helloWorld {
+}
